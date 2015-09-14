b98dbd288290f55037010eea8b0f71236f13dfdf
LinkB3AT <linkb3at@gmail.com>
2015-09-14 15:08:37 +0200

    0.1.5

--- config/database.js
+++ config/database.js
@@ -1,6 +1,18 @@
 // config/database.js
+
+// default to a 'localhost' configuration:
+var connection_string = '127.0.0.1:27017/YOUR_APP_NAME';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
 module.exports = {
 
-    'url' : 'mongodb://admin:PpC42zCIsZE2@$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/nodejs' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : connection_string // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
