012d2cf470ac060c784552ba56edddf4f2cdbdba
LinkB3AT <linkb3at@gmail.com>
2015-09-15 13:47:41 +0200

    0.2.4

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '509725255870749', // your App ID
         'clientSecret'    : 'fb07b9b4bdaddc5d9d8ce0f2084e5d0b', // your App Secret
-        'callbackURL'     : 'http://nodejs-linkb3at.rhcloud.com:8080/auth/facebook/callback'
+        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback'
     },
 
     'twitterAuth' : {
